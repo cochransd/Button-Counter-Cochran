2bd5b2d118f66153344bbb2559cc6106aad10675
cochransd <cochransd@g.cofc.edu>
2017-02-02 12:59:15 -0800

    temp

--- app/src/main/java/steve_cochran/button_counter_cochran/MainActivity.java
+++ app/src/main/java/steve_cochran/button_counter_cochran/MainActivity.java
@@ -2,12 +2,60 @@ package steve_cochran.button_counter_cochran;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+import java.*;
 
 public class MainActivity extends AppCompatActivity {
+
+    public TextView textView;
+    public String stringTv;
+    private Integer count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView)findViewById(R.id.textView2);
+
+        count = 0;
+        textView.setText(count.toString());
+    }
+
+    public void doSomething1(View v){
+
+
+        count = count + 1;
+        textView.setText(count.toString());
+
+
+        Log.d("VIVZ", textView.toString());
+
+    }
+
+
+    public void doSomething2(View v){
+
+        count = count - 1;
+        textView.setText(count.toString());
+
+        Log.d("VIVZ", textView.toString());
+
+    }
+
+    public void doSomething3(View v){
+
+        count = 0;
+        textView.setText(count.toString());
+
+
+        textView.setText("0");
+        Log.d("VIVZ", textView.toString());
+
     }
 }
+
+
